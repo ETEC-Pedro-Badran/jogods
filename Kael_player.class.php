9748974d01fdc993e83a9e543b0bc7d52e739619
Kael <rodriguesana9198@gmail.com>
2022-06-08 13:26:06 -0300

    Kael

---
+++ Kael_player.class.php
@@ -0,0 +1,22 @@
+<?php
+  require_once("player_abstract.class.php");
+
+  class Kael extends AbstractPlayer {
+
+
+    function desenhar(){
+        return "<div>
+        
+            <img src='https://br.pinterest.com/josejoab706/stickman/'>
+        
+        
+        </div>";
+
+
+    }
+
+
+  }
+
+  
+?>
