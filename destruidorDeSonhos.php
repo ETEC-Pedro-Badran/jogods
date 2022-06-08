9c358b520575b991636465730910a15f796cea5d
isabelapomini <isabela.pomini@gmail.com>
2022-06-08 13:31:23 -0300

    DestruidorDeSonhos

---
+++ destruidorDeSonhos.php
@@ -0,0 +1,22 @@
+<?php
+  require_once("player_abstract.class.php");
+
+  class DestruidorDeSonhos extends AbstractPlayer {
+
+
+    function desenhar(){
+        return "<div>
+        
+            <img src='https://br.pinterest.com/pin/611645193131734109/'>
+        
+        
+        </div>";
+
+
+    }
+
+
+  }
+
+  
+?>
