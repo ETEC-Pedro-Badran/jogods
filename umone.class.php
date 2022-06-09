ddfbc5b80c3d816626b1348dcb4e227b5097822e
LuiisOtavio <dolinhofx@gmail.com>
2022-06-09 11:24:08 -0300

    umone

---
+++ umone.class.php
@@ -0,0 +1,22 @@
+<?php
+    require_once("player_abstract.class.php");
+
+    class Umone extends AbstractPlayer {
+ 
+
+        function desenhar(){
+            return "<div>
+            
+                <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png'>
+            
+
+            </div>";
+
+
+        }
+  
+  
+    }
+
+    
+?>
