6930554d597534ab551455c64bf96b1170213faa
CatatauAliberte <hugoalibertesantos@gamil.com>
2022-06-09 11:17:38 -0300

    Catatau

---
+++ Catatau.php
@@ -0,0 +1,22 @@
+<?php
+  require_once("player_abstract.class.php");
+
+  class Catatau extends AbstractPlayer {
+
+
+    function desenhar(){
+        return "<div>
+        
+            <img src='https://pbs.twimg.com/profile_images/728028467799982080/GDYWxs07_400x400.jpg'>
+        
+        
+        </div>";
+
+
+    }
+
+
+  }
+
+  
+?>
