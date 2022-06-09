78d78818948f4a0a08f4250838fc7862373194fc
CatatauAliberte <hugoalibertesantos@gamil.com>
2022-06-09 11:37:43 -0300

    Gabriel

---
+++ gabs.class.php
@@ -0,0 +1,22 @@
+<?php
+  require_once("player_abstract.class.php");
+
+  class Gabriel extends AbstractPlayer {
+
+
+    function desenhar(){
+        return "<div>
+        
+        <img src=' https://aventurasnahistoria.uol.com.br/media/_versions/curiosidades/death_capa_widelg.jpg'>
+        
+        
+        </div>";
+
+
+    }
+
+
+  }
+
+  
+?>
