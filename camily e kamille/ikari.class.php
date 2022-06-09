5db528f28f31f5497f7cfb6ae78bde36d1602407
camily <camily.santos10@etec.sp.gov.br>
2022-06-09 11:11:29 -0300

    Camily e Kamille

---
+++ camily e kamille/ikari.class.php
@@ -0,0 +1,22 @@
+<?php
+  require_once("player_abstract.class.php");
+
+  class Evaldao extends AbstractPlayer {
+
+
+    function desenhar(){
+        return "<div>
+        
+            <img src='https://1.bp.blogspot.com/-1cYsd0R7CZo/WqcgqxjzYUI/AAAAAAAABlU/ElZywHA39mIkz9J_tkyD05v2iRewPnAggCLcBGAs/s1600/Karma_0.jpg'>
+        
+        
+        </div>";
+
+
+    }
+
+
+  }
+
+  
+?>
