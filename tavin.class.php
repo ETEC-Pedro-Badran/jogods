e5d39ce756cb91113d6d1e9a4cc343135ba6c4b6
Taversons <otavioarthur553@gmail.com>
2022-06-13 09:35:33 -0300

    Otavio

---
+++ tavin.class.php
@@ -0,0 +1,22 @@
+<?php
+  require_once("player_abstract.class.php");
+
+  class TAVAS extends AbstractPlayer {
+
+
+    function desenhar(){
+        return "<div>
+        
+            <img src='https://i.pinimg.com/originals/d4/3e/8c/d43e8cc938456ec7be45242695725b6b.jpg'>
+        
+        
+        </div>";
+
+
+    }
+
+
+  }
+
+  
+?>
