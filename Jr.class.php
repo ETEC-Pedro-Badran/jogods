64247e99508ae7906f5f84c807aeb64652a4360c
Lauanevieira2021 <lauane.vieira01@etec.sp.gov.br>
2022-06-15 13:26:51 -0300

    Marilia

---
+++ Jr.class.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+require_once ("player_Abstract.class.php");
+
+class jogador extends AbstractPlayer
+{
+
+
+
+    function desenhar() 
+    {
+          return "<div>
+          
+          <img src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxBXq7MEcOIQw3qQzz4NlOWJ8BhF62vLnOqQ&usqp=CAU'
+          
+          </div>";
+     
+    }
+}
+    ?>
